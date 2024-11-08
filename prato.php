c359df20d1437a56a252d835014a96ce7c3f6374
fernando <fernandofecci@hotmail.com>
2024-11-07 21:55:16 -0300

    aula 1

--- prato.php
+++ prato.php
@@ -1,95 +1,6 @@
-<!doctype html>
-<html class="no-js" lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Restô Bar</title>
-        <link rel="stylesheet" href="css/foundation.css" />
-        <link rel="stylesheet" href="css/slick.css" />
-        <link rel="stylesheet" href="css/style.css" />
-        <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Permanent+Marker|Raleway:400,700" rel="stylesheet">
-        <script src="js/vendor/modernizr.js"></script>
-    </head>
-    
-    <body>
-
-
-		<header>
-			
-			<div class="main-header large-12 columns no-padding">
-
-				<div class="global-page-container">
-				
-					<div class="logo small-6 small-offset-3 large-3 large-offset-0 columns no-padding">
-						<a href="index.html" title="home">
-							<div class="table">
-								<div class="table-cell">
-									<h1>Restô Bar</h1>
-								</div>
-							</div>
-						</a>
-					</div>
-
-					<div class="main-menu show-for-large-up large-9 columns text-right">		
-						<div class="table">
-							<div class="table-cell">
-								<ul class="menu-items">
-									<li><a href="index.html#about-us">Sobre</a></li>
-									<li><a href="cardapio.html">Cardápio</a></li>
-									<li><a href="index.html#contact-us">Reserva</a></li>
-									<li><a href="index.html#footer">Contacto</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					
-					<div class="hamburguer-icon small-2 columns text-right">
-                        <div class="table">
-                            <div class="table-cell">
-                                <img src="img/menu/hamburguer.svg">
-                            </div> 
-                        </div>
-					</div>
-
-					<div class="right-space small-1 columns"></div>
-
-				</div>
-			</div>			
-					
-            <div class="sliding-header-menu-outer">						
-                <div class="sliding-header-menu">
-                    
-                    <div class="sliding-header-menu-close-button small-12 columns">
-                        <div class="table">
-                            <div class="table-cell">
-                                <img class="close-icon" src="img/menu/close.svg">
-                            </div>	
-                        </div>	
-                    </div>
-
-                    
-
-                    <div class="sliding-header-menu-main-menu small-12 columns">
-                        
-                        <div class="table">
-                            <div class="table-cell">
-                                <ul class="sliding-header-menu-li">
-                                    <li><a href="index.html#about-us">Sobre</a></li>
-                                    <li><a href="cardapio.html">Cardápio</a></li>
-                                    <li><a href="index.html#contact-us">Reserva</a></li>
-                                    <li><a href="index.html#footer">Contacto</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        
-                    </div>                           
-                </div>
-            </div>
-
-		</header>
-
-		<div class="ghost-element">
-		</div>
+<?php 
+include 'header.php'
+?>
            
         <div class="product-page small-11 large-12 columns no-padding small-centered">
             
@@ -97,8 +8,8 @@
 
                 <div class="product-section">
                     <div class="product-info small-12 large-5 columns no-padding">
-                        <h3>Salmão aos Legumes</h3>
-                        <h4>Pratos Principais</h4>
+                        <h3>Cheesecake de cereja</h3>
+                        <h4>Sobremesas</h4>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales 
                             justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod 
                             efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit 
@@ -106,12 +17,12 @@
                             tempus ipsum augue, sit amet tristique lacus semper cursus.     
                         </p>
 
-                        <h5><b>Preço: </b>R$ 21</h5>
-                        <h5><b>Calorias: </b>480</h5> 
+                        <h5><b>Preço: </b>R$ 8</h5>
+                        <h5><b>Calorias: </b>423</h5> 
                     </div>
 
                     <div class="product-picture small-12 large-7 columns no-padding">
-                        <img src="img/cardapio/salmao-legumes.jpg" alt="picanha">
+                        <img src="img/cardapio/cheesecake-cereja.jpg" alt="cheesecake de cereja">
                     </div>
 
                 </div>
